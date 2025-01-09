9d123d31fcf57f3c1da6608fedc6a0c3d85cab74
Arnaud <hayon.arnaud@gmail.com>
2025-01-09 12:37:58 +0100

    add: AddCollection Page + remode: TabView in Menu

--- Collection_/Components/CollectionDetail.swift
+++ Collection_/Components/CollectionDetail.swift
@@ -13,28 +13,31 @@ struct CollectionDetail: View {
     
     var body: some View {
         HStack {
-            if let firstVinyl = collection.vinyls.first {
-                NavigationLink(destination: VinylList(collection: $collection)) {
-                    VStack {
-                        AsyncImage(url: URL(string: firstVinyl.cover_image_url)) { image in
-                            image
-                                .resizable()
-                        } placeholder: {
-                            ProgressView()
+            HStack {
+                if let firstVinyl = collection.vinyls.first {
+                    NavigationLink(destination: VinylList(collection: $collection)) {
+                        VStack {
+                            AsyncImage(url: URL(string: firstVinyl.cover_image_url)) { image in
+                                image
+                                    .resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
+                            .background(Color.tileBackground)
+                            .cornerRadius(10)
+                            
+                            Text(collection.name)
+                                .font(.headline)
+                                .foregroundStyle(.black)
+                                .lineLimit(1)
                         }
-                        .frame(width: 100, height: 100)
-                        .background(Color.tileBackground)
-                        .cornerRadius(10)
-
-                      Text(collection.name)
-                            .font(.headline)
-                            .foregroundStyle(.black)
-                            .lineLimit(1)
                     }
                 }
             }
+           
         }
-        .navigationTitle("Mes CollectionDetail")
+        .navigationTitle("Mes Collections")
     }
 }
 //#Preview {
--- Collection_/View/AddCollection.swift
+++ Collection_/View/AddCollection.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct AddCollection: View {
+    
+    @State var collectionName: String = ""
+    @State var collectionType: String = ""
+    
     var body: some View {
-        Text("Ajouter une collection")
+        ScrollView {
+            VStack (alignment: .leading) {
+                TextField("Title", text: $collectionName)
+                    .autocorrectionDisabled()
+                    .textFieldStyle(.roundedBorder)
+                
+                TextField("Type", text: $collectionType)
+                    .autocorrectionDisabled()
+                    .textFieldStyle(.roundedBorder)
+                Spacer()
+                
+                StyledGenericButton(type: .text(value: "Ajouter une collection"))
+            }
+            .frame(minHeight: UIScreen.main.bounds.height - 220) // Donne une hauteur minimale
+
+        }
+        .padding()
+        .navigationTitle("Ajouter une collection")
+
+        
     }
 }
 
